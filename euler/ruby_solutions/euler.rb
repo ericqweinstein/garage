072bb50fbf2d353eb6769942b163d031f46d369d
Eric Weinstein <eric.q.weinstein@gmail.com>
2012-12-01 15:30:02 -0500

    solutions 5, 6, 7

--- euler/ruby_solutions/euler.rb
+++ euler/ruby_solutions/euler.rb
@@ -1,5 +1,5 @@
-# These will all live in one file, since several
-# of the  problems lend themselves to code reuse.
+# These will all live in one file, since I figure
+# the solutions  will lend themselves to code reuse.
 
 # Problem 1: Add all the natural numbers below one
 # thousand that are multiples of three or five.
@@ -70,3 +70,42 @@ def problem_four
 end
 
 # problem_four ==> 906609
+
+#####
+
+# Problem 5: What is the smallest positive number
+# evenly divisible by all the numbers 1 - 20?
+
+require 'rational'
+
+def problem_five
+  answer = (1..20).inject(1) { |res, n| res.lcm n }
+end
+
+# problem_five ==> 232792560
+
+#####
+
+# Problem 6: Find the difference between the
+# sum of the squares of the first one hundred
+# natural numbers and the square of the sum.
+
+def problem_six
+  (1.upto(100).inject(:+))**2 - (1.upto(100).map { |n| n**2 }).inject { |sum, n| sum + n }
+end
+
+# problem_six ==> 25164150
+
+#####
+
+# Problem 7: What is the 10,001st prime number?
+
+require 'mathn'
+
+def problem_seven
+  primes = Prime.new
+  10_000.times { primes.next }
+  primes.next
+end
+
+# problem_seven ==> 104743
