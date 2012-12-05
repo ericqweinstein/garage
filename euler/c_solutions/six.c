aced49bfeba796e3284c9aa7b3ca4f4b57a5f560
Eric Weinstein <eric.q.weinstein@gmail.com>
2012-12-04 21:46:39 -0500

    solution to problem #6

---
+++ euler/c_solutions/six.c
@@ -0,0 +1,44 @@
+/* * * * * * * * * * * * * * * * * *
+ * Project Euler Problem Number Six:
+ *
+ * What is the difference between the
+ * sum of the squares and the square
+ * of the sums for the numbers 1 - 100?
+ * * * * * * * * * * * * * * * * * */
+
+#include <stdio.h>
+
+int squareOfSum(int);
+int sumOfSquares(int);
+
+int main(void)
+{
+  int result = squareOfSum(100) - sumOfSquares(100);
+  printf("%d\n", result);
+  return result;
+
+  // Answer: 25164150
+
+}
+
+int squareOfSum(int limit)
+{
+  int i;
+  int current = 0;
+  for (i = 0; i <= limit; i++)
+  {
+    current += i;
+  }
+  return (current * current);
+}
+
+int sumOfSquares(int limit)
+{
+  int i;
+  int current = 0;
+  for (i = 0; i <= limit; i++)
+  {
+    current += (i * i);
+  }
+  return current;
+}
