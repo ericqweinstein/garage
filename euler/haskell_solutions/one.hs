77eebc209f7ba4f4b15977defbc3acd0fadd547a
Eric Weinstein <eric.q.weinstein@gmail.com>
2012-12-25 12:22:08 -0500

    fixed whitespace

--- euler/haskell_solutions/one.hs
+++ euler/haskell_solutions/one.hs
@@ -4,6 +4,6 @@
 -- 1000 that are multiples of 3 or 5.
 
 problemOne :: Integer -> Integer
-problemOne x = sum [n | n <- [1..x-1] , n `mod` 3 == 0 || n `mod` 5 == 0]
+problemOne x = sum [n | n <- [1..x-1], n `mod` 3 == 0 || n `mod` 5 == 0]
 
 -- problemOne 1000 ==> 233168
