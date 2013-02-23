2fa51d962bf1516e6ae180366b0b27f5b6f5f12e
Eric Weinstein <eric.q.weinstein@gmail.com>
2013-02-23 13:37:48 -0500

    problem 4 solution

---
+++ euler/haskell_solutions/four.hs
@@ -0,0 +1,10 @@
+-- Project Euler Problem Number Four:
+--
+-- Find the largest palindrome made from
+-- the product of two three-digit numbers.
+
+problemFour :: Integer
+problemFour = maximum [x | y <- [100..999], z <- [y..999], let x = y * z, let s = show x, s == reverse s ]
+
+-- problemFour ==> 906609
+
