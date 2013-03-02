655ca6cab0474431d86cc2fc3cab9c139e201aad
Eric Weinstein <eric.q.weinstein@gmail.com>
2013-03-02 18:30:04 -0500

    friendlier line breaks

--- euler/clojure_solutions/one.clj
+++ euler/clojure_solutions/one.clj
@@ -3,7 +3,8 @@
 ; Add all the natural numbers below
 ; 1000 that are multiples of 3 or 5.
 
-(defn problemOne [n] (reduce + (filter #(or (zero? (rem % 3)) (zero? (rem % 5))) (range n))))
+(defn problemOne [n]
+  (reduce + (filter #(or (zero? (rem % 3)) (zero? (rem % 5))) (range n))))
 
 ; (problemOne 1000) ==> 233168
 
--- euler/clojure_solutions/two.clj
+++ euler/clojure_solutions/two.clj
@@ -4,9 +4,11 @@
 ; the Fibonacci sequence below four million.
 
 ; Function for calculating Fibonacci numbers
-(defn fib [a b] (lazy-cat [a] (fib b (+ a b))))
+(defn fib [a b]
+  (lazy-cat [a] (fib b (+ a b))))
 
-(defn problemTwo [n] (reduce + (for [f (fib 1 2) :when (zero? (mod f 2)) :while (< f n)] f)))
+(defn problemTwo [n]
+  (reduce + (for [f (fib 1 2) :when (zero? (mod f 2)) :while (< f n)] f)))
 
 ; (problemTwo 4000000) ==> 4613732
 
