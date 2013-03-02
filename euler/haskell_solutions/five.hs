afa7cc9c450eee44fd9ee467c0849724ca9a32f6
Eric Weinstein <eric.q.weinstein@gmail.com>
2013-03-02 17:34:53 -0500

    whitespace fix

--- euler/haskell_solutions/five.hs
+++ euler/haskell_solutions/five.hs
@@ -4,7 +4,7 @@
 -- by each of the numbers one to twenty?
 
 problemFive :: [Integer] -> Integer
-problemFive xs =  foldr (lcm) 1 xs
+problemFive xs = foldr (lcm) 1 xs
 
 -- problemFive [1..20] ==> 232792560
 
