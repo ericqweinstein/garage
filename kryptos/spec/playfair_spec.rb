66e91caa073d2684ccd3611faaefa3ffaa517001
Eric Weinstein <eric.q.weinstein@gmail.com>
2013-07-13 10:51:04 -0500

    K Я Y P T O S

--- kryptos/spec/playfair_spec.rb
+++ kryptos/spec/playfair_spec.rb
@@ -5,5 +5,43 @@ require_relative '../playfair.rb'
 describe Playfair do
   include Playfair
 
+  before do
+    @grid = [
+      %w[K R Y P T],
+      %w[O S A B C],
+      %w[D E F G H],
+      %w[I J L M N],
+      %w[U V W X Z]
+    ]
+    
+    @ciphertext = 'CYYCOTCYFOZL'
+    @plaintext  = 'ATTACKATDAWN'
+  end
+
+  describe '#build_table' do
+    it 'needs tests' do
+    end
+  end
+  describe '#encrypt' do
+    it 'correctly encrypts plaintext given a grid' do
+      encrypt(@plaintext, @grid).should eq(@ciphertext)
+    end
+  end
+  describe '#decrypt' do
+    it 'needs tests' do
+    end
+  end
+  describe '#bigramify' do
+    it 'should handle even-lettered strings' do
+      bigramify('ELEPHANT').should eq(%w[EL EP HA NT])
+    end
+    it 'should handle odd-lettered strings' do
+      bigramify('PANDA').should eq(%w[PA ND AX])
+    end
+    it 'should handle bigrams with repeated letters' do
+      bigramify('ABOOKKEEPER').should eq(%w[AB OX OK KE EP ER])
+      bigramify('GIRAFFE').should eq(%w[GI RA FX FE])
+    end
+  end
 end
 
