c018afb38ba9fe2d3c51909a8b736342278d5796
Eric Weinstein <eric.q.weinstein@gmail.com>
2013-07-13 00:31:34 -0500

    K Я Y P T O S

---
+++ kryptos/spec/playfair_spec.rb
@@ -0,0 +1,9 @@
+# encoding: UTF-8
+
+require_relative '../playfair.rb'
+
+describe Playfair do
+  include Playfair
+
+end
+
---
+++ kryptos/spec/transposition_spec.rb
@@ -0,0 +1,9 @@
+# encoding: UTF-8
+
+require_relative '../transposition.rb'
+
+describe Transposition do
+  include Transposition
+
+end
+
