332f2dd32909ff2794c7076e195fc7a3d8f4c20a
Eric Weinstein <eric.q.weinstein@gmail.com>
2013-07-24 17:45:27 -0400

    K Я Y P T O S

--- kryptos/lib/dekryptos/transposition.rb
+++ kryptos/lib/dekryptos/transposition.rb
@@ -12,39 +12,52 @@ module Transposition
 
   # Public: encrypts text via keyed columnar transposition.
   #
-  # Notes TK
+  # The encryption method converts the plaintext to an array,
+  # each row within which comprises +width+ characters. The
+  # array is then transposed and each row is reversed, after
+  # which the array is converted back to a string.
   #
   # === Parameter(s)
   # +plaintext+ - +String+: the text to be encrypted.
   # +width+ - +Integer+: the width of each row in the intermediary
-  # table required for transposition. (If the number of characters
-  # in the plaintext is not a multiple of the width, the last row
-  # will be shorter than the rest of the rows.)
+  # table required for transposition.
   #
   # === Return Value
   # +String+: the encrypted text.
   #
   # === Example
-  # TK
+  # Assuming +pt+ is the plaintext version of K3:
+  # +encrypt(encrypt(pt, 7), 84)+
+  # +=> # Results in K3+
   def encrypt(plaintext, width)
+    matrix = plaintext.chars.each_slice(width).map(&:join)
+    matrix.each_with_index do |row, index|
+      matrix[index] = matrix[index].split('')
+    end
+
+    matrix.transpose.map(&:reverse).join('')
   end
   
   # Public: decrypts text via keyed columnar transposition.
   #
-  # Notes TK
+  # The decryption method converts the ciphertext to an array,
+  # each row within which comprises +width+ characters. Each
+  # row in the array is reversed and the entire array is
+  # transposed, after which the array is converted back to a
+  # string.
   #
   # === Parameter(s)
   # +ciphertext+ - +String+: the text to be decrypted.
   # +width+ - +Integer+: the width of each row in the intermediary
-  # table required for transposition. (If the number of characters
-  # in the ciphertext is not a multiple of the width, the last row
-  # will be shorter than the rest of the rows.)
+  # table required for transposition.
   #
   # === Return Value
   # +String+: the decrypted text.
   #
   # === Example
-  # TK
+  # Assuming +K3+ is part 3 of the Kryptos sculpture:
+  # +decrypt(decrypt(K3, 4), 48)+
+  # +=> # Results in the plaintext+
   def decrypt(ciphertext, width)
     matrix = ciphertext.chars.each_slice(width).map(&:join)
     matrix.each_with_index do |row, index|
